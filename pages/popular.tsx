83a13ccadfb7c60801c2c01109c10d051d5d2385
Ivan Mukoed <1rydler@gmail.com>
2021-11-19 11:50:56 +0200

    Changed API

--- pages/popular.tsx
+++ pages/popular.tsx
@@ -1,21 +1,26 @@
 import { useEffect, useState } from 'react'
 import Link from "next/link"
+import { HiArrowCircleLeft, HiArrowCircleRight } from 'react-icons/hi'
+import axios from 'axios';
 
 const popular = () => {
     const [posts, setPosts] = useState([])
+    const [page, setPage] = useState(1)
 
-    const fetchData = async () => {
-        const res = await fetch(`http://api.mediastack.com/v1/news?access_key=5196326fd27149303d15d7c09e39ca0e&languages=en&sort=popularity`)
-        const data = await res.json()
-
-        console.log(data.data)
-
-        setPosts(data.data)
+    const fetchData = () => {
+        axios.get(`https://api.newscatcherapi.com/v2/latest_headlines?lang=en&page=${page}&page_size=20`, {
+            headers: {
+                "x-api-key": "7tWWmBC8whwJyMDpp9v68FnDaUYgqeET6M8k1-J_ALA"//the token is a variable which holds the token
+            }
+        }).then((data) => {
+            console.log(data.data.articles)
+            setPosts(data.data.articles)
+        })
     }
 
     useEffect(() => {
         fetchData()
-    }, [])
+    }, [page])
 
     return (
         <div className="text-center py-5">
@@ -23,18 +28,27 @@ const popular = () => {
             <div className="flex flex-wrap justify-around">
                 {!posts ? "No news were found! We're sorry..." : posts.map((post, idx) => {
                     return (
-                        <div key={idx} className="bg-gray-200 text-center max-w-sm w-auto rounded-lg my-5 pb-10">
+                        <div className="bg-gray-200 text-center max-w-sm w-auto rounded-lg my-5 pb-10">
                             <div className="h-48 w-96 pb-5">
-                                <img className="max-w-sm min-w-full rounded-t-lg max-h-48 min-h-full" src={post.image ? post.image : "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png"}></img>
+                                <img className="max-w-sm min-w-full rounded-t-lg max-h-48 min-h-full" src={post.media ? post.media : "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png"}></img>
                             </div>
                             <div className="font-bold text-lg p-5">{post.title}</div>
-                            <div className="px-5 pb-5">{post.description}</div>
-                            <Link href={post.url}>
+                            <div className="px-5 pb-5">{post && post.summary && post.summary.length > 200 ? `${post.summary.substring(0, 200)}...` : ""}</div>
+                            <Link href={post.link}>
                                 <a className="transition bg-gray-800 cursor-pointer hover:bg-gray-900 text-white py-2 px-6 rounded mx-2">Learn More</a>
                             </Link>
                         </div>
                     )
                 })}
+            </div>
+            <div className="flex pb-10 justify-center">
+                <div className="pt-1 cursor-pointer" onClick={() => {
+                    setPage(page - 1)
+                }}>{page === 1 ? "" : <HiArrowCircleLeft />} </div>
+                <div className="px-5 mb-10 pt-px">{page}</div>
+                <div className="pt-1 cursor-pointer" onClick={() => {
+                    setPage(page + 1)
+                }}><HiArrowCircleRight /></div>
             </div>
 
         </div>
